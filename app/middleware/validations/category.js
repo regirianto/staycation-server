6db446d9bd76aedf7b54d49dd4578d46ab03cdaa
unknown <regirianto22@gmail.com>
2022-03-11 13:33:48 +0700

    fix typo validation category

--- app/middleware/validations/category.js
+++ app/middleware/validations/category.js
@@ -2,7 +2,6 @@ const { body } = require("express-validator");
 
 const createCategoryValidation = [
   body("name").contains().withMessage("Category is required"),
-  body,
 ];
 
 module.exports = { createCategoryValidation };
