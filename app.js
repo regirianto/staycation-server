1f87d05ebbf66aa33b25a00da1bc8d2998ae31a7
unknown <regirianto22@gmail.com>
2022-03-11 13:22:13 +0700

    add validation featured

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(flash());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
-// app.use(logger("dev"));cl
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
